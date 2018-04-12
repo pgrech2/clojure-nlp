db269e2f379c2829c59b4f270e4281e800b594dc
Paul Grech <pgrech@theladders.com>
2018-04-12 16:38:45 -0400

    Refactor of svm.clj

--- src/model/svm.clj
+++ src/model/svm.clj
@@ -3,52 +3,37 @@
   (:import [libsvm svm_node svm_parameter svm_problem svm])
   (:require [clojure.edn :as edn]
             [clojure.java.io :refer [file reader writer]]
-            [clojure.string :refer [join replace split]]))
+            [clojure.string :as cstr]))
 
 
 ;; Thanks to:
 ;; https://github.com/r0man/svm-clj/blob/master/src/svm/core.clj
 
-(def kernel-types
-  {:linear       svm_parameter/LINEAR
-   :poly         svm_parameter/POLY
-   :pre-computed svm_parameter/PRECOMPUTED
-   :rbf          svm_parameter/RBF
-   :sigmoid      svm_parameter/SIGMOID})
+(defn get-kernel
+  [kernel]
+  (get {:linear       svm_parameter/LINEAR
+        :poly         svm_parameter/POLY
+        :pre-computed svm_parameter/PRECOMPUTED
+        :rbf          svm_parameter/RBF
+        :sigmoid      svm_parameter/SIGMOID} kernel))
 
-(def svm-types
-  {:c-svc       svm_parameter/C_SVC
-   :epsilon-svr svm_parameter/EPSILON_SVR
-   :nu-svc      svm_parameter/NU_SVC
-   :nu-svr      svm_parameter/NU_SVR
-   :one-class   svm_parameter/ONE_CLASS})
+(defn get-svm
+  [svm]
+  (get {:c-svc       svm_parameter/C_SVC
+        :epsilon-svr svm_parameter/EPSILON_SVR
+        :nu-svc      svm_parameter/NU_SVC
+        :nu-svr      svm_parameter/NU_SVR
+        :one-class   svm_parameter/ONE_CLASS} svm))
 
-(def default-params
-  {:C            1
-   :cache-size   100
-   :coef0        0
-   :degree       3
-   :eps          1e-3
-   :gamma        0
-   :kernel-type  (:rbf kernel-types)
-   :nr-weight    0
-   :nu           0.5
-   :p            0.1
-   :probability  0
-   :shrinking    1
-   :svm-type     (:c-svc svm-types)
-   :weight       (double-array 0)
-   :weight-label (int-array 0)})
+(defn feature-counts
+  "Return set of unique feature counts for all records."
+  [records]
+  (set (map (comp count last) records)))
 
-(defn- max-features [dataset]
-  (apply max (mapcat (comp keys last) dataset)))
-
-(defn format-line
-  "Format data as a LibSVM text line."
-  [[label data]]
-  (str label " " (->> (sort data)
-                      (map #(join ":" %))
-                      (join " "))))
+(defn max-features
+  "Return maximum feature count for all records"
+  [records]
+  (apply max (feature-counts records)))
 
 (defn make-node
   "Make a LibSVM node."
@@ -58,87 +43,72 @@
     (set! (. node value) value)
     node))
 
-(defn make-nodes
+(defn svm-node
   "Make a seq of LibSVM nodes."
-  [[label data]] (map #(apply make-node %) data))
+  [[label features]]
+  (map (partial apply make-node) features))
 
-(defn make-params
-  "Make trainer params from `dataset` and `options`."
-  [dataset & {:as options}]
-  (let [params (svm_parameter.)]
-    (doseq [[key val] (merge default-params options)]
-      (clojure.lang.Reflector/setInstanceField params (replace (name key) "-" "_") val))
-    (set! (.gamma params) (/ 1.0 (max-features dataset)))
-    params))
+(defn svm-params
+  "Return SVM parameters."
+  [max-features parameters]
+  (let [{:keys [kernel-type svm-type]} parameters
+        all-params (cond-> (merge {:C            1
+                                   :cache-size   100
+                                   :coef0        0
+                                   :degree       3
+                                   :eps          1e-3
+                                   :gamma        0
+                                   :kernel-type  (get-kernel :rbf)
+                                   :nr-weight    0
+                                   :nu           0.5
+                                   :p            0.1
+                                   :probability  0
+                                   :shrinking    1
+                                   :svm-type     (get-svm :c-svc)
+                                   :weight       (double-array 0)
+                                   :weight-label (int-array 0)}
+                                  parameters)
+                     kernel-type (assoc :kernel-type
+                                        (get-kernel kernel-type))
+                     svm-type    (assoc :svm-type
+                                        (get-svm svm-type)))
+        svm-params (svm_parameter.)]
+    (doseq [[k v] all-params]
+      (clojure.lang.Reflector/setInstanceField
+       svm-params (cstr/replace (str k) #"-" "_") v))
+    (set! (.gamma svm-params)
+          (/ 1.0 max-features))
+    svm-params))
 
-(defn sparse-map
-  "Returns a sparse map of features from seq.  The map will be suitable
-  as the second entry in a dataset vector.  seq should have length
-  equal to the total number of features in the dataset and set empty
-  values to nil.
-  Ex. (sparse-map [5 nil 7]) -> {1 5, 3 7}"
-  [seq]
-  (->> seq
-       (map vector (rest (range)))
-       (keep (fn [[i v]] (when v [i v])))
-       (into (sorted-map))))
 
-(defn make-problem
+(defn svm-problem
   "Make a LibSVM problem from `dataset`."
-  [dataset]
+  [records]
   (let [problem (svm_problem.)]
-    (set! (. problem l) (count dataset))
-    (set! (. problem y) (double-array (map first dataset)))
-    (set! (. problem x) (into-array (map into-array (map make-nodes dataset))))
+    (set! (. problem l) (count records))
+    (set! (. problem y) (-> (map first records)
+                            (double-array)))
+    (set! (. problem x) (->> (map svm-node records)
+                             (map into-array)
+                             (into-array)))
     problem))
 
-(defn parse-line
-  "Parse a LibSVM formatted text `line`."
-  [line]
-  (let [[label & data] (split line #"\s+")]
-    [(edn/read-string label)
-     (reduce
-      (fn [data [index value]]
-        (assoc data
-          (Integer/parseInt index)
-          (Double/parseDouble value)))
-      (sorted-map) (map #(split % #":") data))]))
+(defn train
+  "Train a SVM model with `records` according to optional `parameters`
+
+   Record input format `[label [features]]`"
+  ([records]
+   (train records nil))
+  ([records parameters]
+   (let [svm-params (svm-params (max-features records)
+                                parameters)
+         problem    (svm-problem records)]
+     (svm/svm_check_parameter problem svm-params)
+     (svm/svm_train problem params))))
 
 (defn predict
   "Predict the label of the `feature` with `model`."
   [model feature]
-  (->> (make-nodes [nil feature])
+  (->> (svm-node [nil feature])
        (into-array)
        (svm/svm_predict model)))
-
-(defn read-dataset
-  "Read the dataset in LibSVM format from `url`."
-  [url] (map parse-line (line-seq (reader url))))
-
-(defn read-model
-  "Read the SVM model in LibSVM format from `filename`."
-  [filename] (svm/svm_load_model filename))
-
-(defn train-model
-  "Train a SVM model with `dataset` according to `options`."
-  [dataset & options]
-  (let [problem (make-problem dataset)
-        params (apply make-params dataset options)]
-    (svm/svm_check_parameter problem params)
-    (svm/svm_train problem params)))
-
-(defn write-dataset
-  "Write the `dataset` in LibSVM format to `filename`."
-  [dataset filename]
-  (let [file (file filename)]
-    (.mkdirs (.getParentFile file))
-    (with-open [w (writer file)]
-      (doseq [line (map format-line dataset)]
-        (.write w (str line "\n"))))
-    (str file)))
-
-(defn write-model
-  "Write the `model` in LibSVM format to `filename`."
-  [model filename]
-  (svm/svm_save_model filename model)
-  filename)
